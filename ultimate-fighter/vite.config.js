dc86fb4405751324e71f2990b181afeb2af06298
ShaheeraSadia <Shaheerasadia@gmail.com>
2025-12-03 05:41:51 -0800

    Update vite.config.js

--- ultimate-fighter/vite.config.js
+++ ultimate-fighter/vite.config.js
@@ -6,8 +6,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 📢 THE FIX: Set the base public path for the build
-  base: './', // Use relative path for all assets (this is the most robust fix)
+  // 📢 THE FIX: Use the absolute root path for asset loading
+  base: '/', 
 
   root: './',
   build: {
